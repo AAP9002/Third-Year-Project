5703b960d51ec13f00d632f3379f4d9d0354bd3c
aap9002 <aap9002@gmail.com>
2024-10-11 00:24:33 +0100

    refactor ImageMasks maskImageAboveY

--- data-cleaning/feature/lane-detection/actions/ImageMasks.py
+++ data-cleaning/feature/lane-detection/actions/ImageMasks.py
@@ -1,3 +1,14 @@
+import cv2
+
+
 class ImageMasks:
-    def maskImageAboveY(image, y_index):
+    def maskImageAboveY(image:cv2.typing.MatLike, y_index:int):
+        """mask all pixels above a given y value
+
+        Args:
+            image (MatLike): image passed by reference
+            y_index (Integer): y value to mask at 
+        """
+        if(y_index > image.shape[0]):
+            raise Exception(f'maskImageAboveY y_index out of rank for mask (y_index: {y_index}, image height: {image.shape[0]})')
         image[0:y_index, :] = 0
