fb657463bed9ee92458a32e0c41ca0371414736b
aap9002 <aap9002@gmail.com>
2024-10-11 01:45:49 +0100

    add closing dilate and erode method

--- data-cleaning/feature/lane-detection/actions/SmoothingMethods.py
+++ data-cleaning/feature/lane-detection/actions/SmoothingMethods.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 class SmoothingMethods:
@@ -10,3 +11,8 @@ class SmoothingMethods:
             GAUSSIAN_BLUR_KERNEL_SIZE (tuple[int, int], optional): kernel size to apply on image. Defaults to (9,9).
         """
         cv2.GaussianBlur(image,GAUSSIAN_BLUR_KERNEL_SIZE,0,image)
+
+    def applyClosing(image:cv2.typing.MatLike):
+        kernel = np.ones((5, 5), np.uint8)
+        cv2.dilate(image, kernel, image)
+        cv2.erode(image, kernel, image)
