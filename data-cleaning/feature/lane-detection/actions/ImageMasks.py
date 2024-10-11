d36712f19542eeafa845b77ff6ab5a99153cc9e8
Alan <AAP9002@gmail.com>
2024-10-11 01:48:15 +0100

    TYP18 - lane detection clean frame data and apply canny edge detection (#5)
    * add mask by colour
    
    * add closing dilate and erode method
    
    * add colour thesholding to pipeline

--- data-cleaning/feature/lane-detection/actions/ImageMasks.py
+++ data-cleaning/feature/lane-detection/actions/ImageMasks.py
@@ -12,3 +12,8 @@ class ImageMasks:
         if(y_index > image.shape[0]):
             raise Exception(f'maskImageAboveY y_index out of rank for mask (y_index: {y_index}, image height: {image.shape[0]})')
         image[0:y_index, :] = 0
+
+    def maskByColour(bgr_image:cv2.typing.MatLike, lower_range:int, upper_range:int) -> cv2.typing.MatLike:
+        hsv = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower_range, upper_range)
+        return mask
