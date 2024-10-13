c15b6fec9063d565622a76622e093ca9a42d4cb8
aap9002 <aap9002@gmail.com>
2024-10-13 12:50:51 +0100

    init control window class

---
+++ data-cleaning/controls/ControlWindow.py
@@ -0,0 +1,12 @@
+import cv2
+class ControlWindow:
+    def __init__(self, parent, title):
+        pass
+    
+    def add_button(self, text, callback):
+        pass
+
+    def add_slider(self, text, callback):
+        pass
+
+
