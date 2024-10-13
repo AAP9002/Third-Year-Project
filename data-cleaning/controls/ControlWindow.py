f79f83836a2c81d6c5ea099855e89d648ebaf9e0
aap9002 <aap9002@gmail.com>
2024-10-13 14:49:34 +0100

    init control window

--- data-cleaning/controls/ControlWindow.py
+++ data-cleaning/controls/ControlWindow.py
@@ -1,12 +1,25 @@
-import cv2
+import tkinter as tk
+from tkinter import ttk
+
 class ControlWindow:
     def __init__(self, parent, title):
-        pass
-    
+        self.parent = parent
+        self.root = tk.Tk()
+        self.root.title(title)
+        self.frame = ttk.Frame(self.root, padding="10")
+        self.frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+
     def add_button(self, text, callback):
-        pass
+        button = ttk.Button(self.frame, text=text, command=callback)
+        button.grid(sticky=(tk.W, tk.E))
 
-    def add_slider(self, text, callback):
-        pass
+    def add_slider(self, text, max_val, callback):
+        label = ttk.Label(self.frame, text=text)
+        label.grid(sticky=(tk.W, tk.E))
+        slider = ttk.Scale(self.frame, from_=0, to=max_val, orient=tk.HORIZONTAL, command=lambda val: callback(int(float(val))))
+        slider.grid(sticky=(tk.W, tk.E))
 
-
+    def start(self):
+        self.root.mainloop()
