868335724663bcaecb7081ade5a9b2ff2986f854
aap9002 <aap9002@gmail.com>
2024-10-14 12:59:37 +0100

    add angle filtering for hough lines

--- data-cleaning/feature/LaneDetection/actions/LineMethods.py
+++ data-cleaning/feature/LaneDetection/actions/LineMethods.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+def angle_from_north(x1, y1, x2, y2):
+    return np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi
 
 class LineMethods:
     def applyHoughLines(image: cv2.typing.MatLike, threshold:int=50, minLineLength:int=50, maxLineGap:int=20):
@@ -12,7 +14,9 @@ class LineMethods:
         if lines is not None:
             for line in lines:
                 x1, y1, x2, y2 = line[0]
-                cv2.line(line_image, (x1, y1), (x2, y2), (155, 0, 0), 5)
+                # filter for vertical lines
+                if abs(angle_from_north(x1, y1, x2, y2)) > 23:
+                    cv2.line(line_image, (x1, y1), (x2, y2), (155, 0, 0), 5)
                     
 
         return line_image
