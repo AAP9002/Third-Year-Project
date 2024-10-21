82f2479da56b52c0a1330303a1c8915d94f69dde
aap9002 <aap9002@gmail.com>
2024-10-21 11:16:00 +0100

    run pipeline once per image

--- data-cleaning/main.py
+++ data-cleaning/main.py
@@ -1,4 +1,3 @@
-from multiprocessing import process
 import cv2
 import numpy as np
 import copy
@@ -10,12 +9,21 @@ import Capture.VideoFrameHandler as VideoFrameHandler
 # VideoFrameHandler = VideoFrameHandler.VideoFrameHandler('../data/Know you-re protected 1080p webloop.mp4')
 # VideoFrameHandler = VideoFrameHandler.VideoFrameHandler('../data/10 sec video 1 mototrway crash h.264.mp4')
 # VideoFrameHandler = VideoFrameHandler.VideoFrameHandler('../data/Harard Warning Lights.mp4')
-VideoFrameHandler = VideoFrameHandler.VideoFrameHandler('../data/20241004_182941000_iOS.mp4')
-# VideoFrameHandler = VideoFrameHandler.VideoFrameHandler('../data/2022_0813_184754_009.MP4')
+# VideoFrameHandler = VideoFrameHandler.VideoFrameHandler('../data/20241004_182941000_iOS.mp4')
+VideoFrameHandler = VideoFrameHandler.VideoFrameHandler('../data/2022_0813_184754_009.MP4')
 
-def set_frame(frame_number:int):
+def set_frame(coloured_in:cv2.typing.MatLike):
     global coloured
-    coloured = VideoFrameHandler.get_frame(frame_number)
+    coloured = coloured_in
+
+    processed_image = copy.deepcopy(coloured)
+
+    lanes_overlay = detect_lanes()
+    processed_image = cv2.addWeighted(processed_image, 0.8, lanes_overlay, 1, 1)
+    
+    detect_features()
+
+    cv2.imshow("Original", processed_image)
 
 # get image
 coloured = VideoFrameHandler.get_frame(0)
@@ -26,33 +34,31 @@ play_video = True
 
 def detect_lanes():
     cv2.namedWindow("Original", cv2.WINDOW_NORMAL)
-    processed_image = copy.deepcopy(coloured)
     laneDetectionLine = LaneDetection.LaneDetection.run_pipeline(copy.deepcopy(coloured))
     black_white_to_red = cv2.cvtColor(laneDetectionLine, cv2.COLOR_GRAY2BGR)
     black_white_to_red[:, :, 1] = 0
-    processed_image = cv2.addWeighted(processed_image, 0.8, black_white_to_red, 1, 1)
-    cv2.imshow("Original", processed_image)
+    return black_white_to_red
 
-
-while(True):
-    # detect_lanes()
-
-    # apply sift
+def detect_features():
     sift_image = copy.deepcopy(coloured)
     featureDetectionHandler.detect_features_by_SIFT(sift_image)
 
+
+while(True):
     if play_video:
-        coloured = VideoFrameHandler.get_next_frame()
+        # apply sift
+        set_frame(VideoFrameHandler.get_next_frame())
 
     k = cv2.waitKey(33)
     # print(k)
     if k==27: # Esc key
         break # stop
     elif k == 83: # right
-        coloured = VideoFrameHandler.get_next_frame()
+        set_frame(VideoFrameHandler.get_next_frame())
+
         pass # next image
     elif k == 81: # left
-        coloured = VideoFrameHandler.get_previous_frame()
+        set_frame(VideoFrameHandler.get_previous_frame())
         pass
     elif k == 32: # space
         play_video = not play_video
