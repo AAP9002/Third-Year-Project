5bf977551988efd9ae66d3e99badadb43393826e
aap9002 <aap9002@gmail.com>
2024-11-16 13:05:04 +0000

    add video player

--- Solutions/label-R-VP-dataset/main.py
+++ Solutions/label-R-VP-dataset/main.py
@@ -1,6 +1,7 @@
 ### Open a video and label the frames with the VP dataset
 import tkinter as tk
 from tkinter import filedialog
+import cv2
 
 def getFileName():
     # open file dialog
@@ -9,8 +10,35 @@ def getFileName():
     file_path = filedialog.askopenfilename()
     return file_path
 
+def getCV2Video(filename):
+    # open video file
+    cap = cv2.VideoCapture(filename)
+    return cap
+
+def getFrameCount(cap):
+    # get total number of frames
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    return frame_count
+
+def getFrame(cap, index):
+    # get frame at index
+    cap.set(cv2.CAP_PROP_POS_FRAMES, index)
+    ret, frame = cap.read()
+    return frame
+
 def main():
     file_path = getFileName()
     print(f'file_path: {file_path}')
+    cap = getCV2Video(file_path)
+    frame_count = getFrameCount(cap)
+    
+    for i in range(frame_count):
+        frame = getFrame(cap, i)
+        cv2.imshow('frame', frame)
+        
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 main()
