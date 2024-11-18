2333398a0df57d6ec55d54715ce2aae606a16c1e
AAP9002 <aap9002@gmail.com>
2024-11-18 01:34:16 +0000

    add mouse click to record a new label VP

--- Solutions/label-R-VP-dataset/main.py
+++ Solutions/label-R-VP-dataset/main.py
@@ -1,7 +1,12 @@
 ### Open a video and label the frames with the VP dataset
+from calendar import c
+from time import sleep
 import tkinter as tk
 from tkinter import filedialog
 import cv2
+
+vp_positions = []
+current_frame = 0
 
 def getFileName():
     # open file dialog
@@ -26,19 +31,35 @@ def getFrame(cap, index):
     ret, frame = cap.read()
     return frame
 
+def getCursorPosition(event, x, y, flags, param):
+    global current_pos
+    global current_frame
+    global vp_positions
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        current_frame += 10
+        vp_positions.append((x, y))
+        print(f'vp_positions: {vp_positions}')
+
 def main():
     file_path = getFileName()
     print(f'file_path: {file_path}')
     cap = getCV2Video(file_path)
     frame_count = getFrameCount(cap)
-    
-    for i in range(frame_count):
-        frame = getFrame(cap, i)
+
+    while current_frame < frame_count:
+        frame = getFrame(cap, current_frame)
+        cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+        cv2.setMouseCallback('frame', getCursorPosition)
         cv2.imshow('frame', frame)
-        
-    
-    cap.release()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    print(f'vp_positions: {vp_positions}')
     cv2.destroyAllWindows()
+    cap.release()
+
 
 
 main()
