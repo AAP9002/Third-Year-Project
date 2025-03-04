94271016804a0b6cb412d9c73496cf0b2921fa7c
aap9002 <aap9002@gmail.com>
2025-03-04 04:35:50 +0000

    fix batch harness

--- Solutions/extract_gps_data/batch_harness.py
+++ Solutions/extract_gps_data/batch_harness.py
@@ -1,7 +1,19 @@
 import os
 
-BATCH_FOLDER = '/home/aap9002/Stereo-Road-Curvature-Dashcam/unlabelled/sub'
-OUTPUT_FOLDER = '/home/aap9002/Stereo-Road-Curvature-Dashcam/unlabelled/sub'
+from matplotlib.pylab import f
+
+# BATCH_FOLDER = '/home/aap9002/Stereo-Road-Curvature-Dashcam/unlabelled/sub'
+# OUTPUT_FOLDER = '/home/aap9002/Stereo-Road-Curvature-Dashcam/unlabelled/sub'
+
+# BATCH_FOLDER = '/media/aap9002/Archive 1/Project Videos'
+# OUTPUT_FOLDER = '/media/aap9002/Archive 1/Project Videos'
+
+BATCH_FOLDER = '/home/aap9002/Downloads/dashcam/VIDEO'
+OUTPUT_FOLDER = '/home/aap9002/Downloads/dashcam/VIDEO'
+
+# BATCH_FOLDER = '/home/aap9002/Downloads/dashcam/PROTECTED'
+# OUTPUT_FOLDER = '/home/aap9002/Downloads/dashcam/PROTECTED'
+
 
 SCRIPT_PATH = "bend_extract.py"
 
@@ -11,7 +23,7 @@ def get_video_files(folder):
         full_path = os.path.join(folder, item)
         if os.path.isdir(full_path):
             video_files.extend(get_video_files(full_path))
-        elif full_path.endswith('.mp4') or full_path.endswith('.MP4'):
+        elif full_path.endswith('FH.mp4') or full_path.endswith('FH.MP4'):
             video_files.append(full_path)
     return video_files
 
@@ -20,11 +32,15 @@ def get_file_name_from_path(file_path):
     return os.path.splitext(os.path.basename(file_path))[0]
 
 def get_unique_output_folder(output_folder, video_file):
+    splits = video_file.split('_')
+    if len(splits) > 1:
+        return os.path.join(output_folder, f"{splits[0]}_{splits[1]}")
+
     return os.path.join(output_folder, get_file_name_from_path(video_file))
 
 def build_command(video_file, unique_video_output_folder):
     os.makedirs(unique_video_output_folder, exist_ok=True)
-    return f"python {SCRIPT_PATH} --input {video_file} --output {unique_video_output_folder}"
+    return f"python {SCRIPT_PATH} --i '{video_file}' --o '{unique_video_output_folder}'"
 
 def main():
     print("finding video files in folder: ", BATCH_FOLDER)
